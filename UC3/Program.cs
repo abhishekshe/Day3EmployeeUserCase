a7528eb02d29ca56f206291dd58362d5298d1eca
Abhishe <abhisheknarwariya813@gmail.com>
2022-03-12 23:42:18 +0530

    added UC3

---
+++ UC3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PartTime = 1;
+            int FullTime = 2;
+            int EmpRatePerHr = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == PartTime)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == FullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWages = empHrs * EmpRatePerHr;
+            Console.WriteLine("emm Wages:" + empWages);
+        }
+    }
+}
